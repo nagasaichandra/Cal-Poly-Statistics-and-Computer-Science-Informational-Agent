df64b4a86acff4d87dac0ec3b3a61a44691e9da4
William Ritson <william.ritson@gmail.com>
2019-05-20 12:41:08 -0700

    Added a stub for discord chat.
    Should be intercompatable with cli chat.

---
+++ src/discord-chat-interface.py
@@ -0,0 +1,13 @@
+import unittest
+
+class DiscordChatInterface:
+    def __init__():
+        pass
+
+class TestDiscordChatInterface(unittest.TestCase):
+    def test_failure(self):
+        # Replace this with actual tests
+        self.assertEqual(True, False)
+
+if __name__ == "__main__":
+    unittest.main()
