9ef1aab68ac1cd8b35f9b1b91aeb1395378f9fa6
William Ritson <william.ritson@gmail.com>
2019-05-27 14:29:41 -0700

    Added question parsing utility.

---
+++ src/parse-questions.py
@@ -0,0 +1,48 @@
+import re
+import sys
+
+variable_regex = re.compile(r'\[([^\]]+)\]')
+question_regex = re.compile('[0-9]+\.\s*([^\n]+)')
+
+
+def normalize_variable_name(var):
+    """ Normalizes a variable name to reduce the chance of the same variable having multiple names """
+    return var.lower().replace(' ', '-')
+
+
+def get_all_variables(text):
+    """ Returns all variables within a section of text (variables are denoted by brackets []) """
+    return sorted(set((normalize_variable_name(var) for var in re.findall(variable_regex, text))))
+
+
+def get_all_questions(text):
+    """ Finds all the questions in a section of text. A question is a line of text that starts with a number """
+    return re.findall(question_regex, text)
+
+
+def reformat(questions, variables):
+    """ Converts a list of questions and variables into formated text. 
+    The variables in the questions will be normalized and then listed out (awaiting description comments for each) """
+    formated_questions = '\n'.join((re.sub(variable_regex, lambda match: normalize_variable_name(
+        match.group(0)), question) for question in questions))
+    formated_variables = '\n'.join(('[{}]: '.format(var) for var in variables))
+    return '{}\n\n{}'.format(formated_questions, formated_variables)
+
+
+def reformat_file(input_filepath, output_filepath):
+    """ Converts a file containing questions into a normalized output file with questions and variables """
+    file_text = open(input_filepath).read()
+    questions = get_all_questions(file_text)
+    variables = get_all_variables(file_text)
+    open(output_filepath, 'w').write(reformat(questions, variables))
+
+
+def main():
+    args = sys.argv[1:]
+    if not args or len(args)  < 2:
+        print("usage: inputfile outputfile")
+        sys.exit(1)
+    reformat_file(args[0], args[1])
+
+if __name__ == '__main__':
+    main()
