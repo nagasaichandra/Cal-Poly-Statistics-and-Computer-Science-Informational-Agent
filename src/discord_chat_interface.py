64dc426d5145a7f1c400c413db8e42727e20a715
William Ritson <william.ritson@gmail.com>
2019-05-27 22:18:32 -0700

    Added discord example code.

--- src/discord_chat_interface.py
+++ src/discord_chat_interface.py
@@ -1,8 +1,19 @@
 import unittest
 
-class DiscordChatInterface:
-    def __init__(self):
-        pass
+import discord
+
+class DiscordChatInterface(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
 
 class TestDiscordChatInterface(unittest.TestCase):
     def test_failure(self):
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from discord_chat_interface import DiscordChatInterface
+
+client = DiscordChatInterface()
+client.run('NTgyNzk1NjcxOTY3NDk4MjUx.XOzByQ.TWGoeHzh5i-LI4dVLWUFmrMmJ5w')
