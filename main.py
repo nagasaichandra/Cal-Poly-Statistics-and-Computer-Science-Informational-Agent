f9c341e8d5d75529d6a39825addc22670ba977e2
William Ritson <william.ritson@gmail.com>
2019-05-30 11:39:24 -0700

    Fixed typos and formatting issues.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import sys
 
 rd = RelevanceDetector()
 
+
 def answer_query(query):
     start_time = time.time()
     matched_question, matched_answer = rd.most_relevant_query(query)
@@ -24,37 +25,35 @@ def answer_query(query):
     print("Saved query in", time.time() - start_time)
     return matched_answer
 
+
 def get_feedback(query):
-    feeback_query = "UPDATE user_query SET correct = %s ORDER BY time_asked DESC LIMIT 1;"
+    feedback_query = "UPDATE user_query SET correct = %s ORDER BY time_asked DESC LIMIT 1;"
     query = query.lower().strip('.')
     if query == "yes" or query == "correct" or query == "right" or query == "good bot":
         with connection.cursor() as cursor:
-            cursor.execute(feeback_query, True)
+            cursor.execute(feedback_query, True)
             connection.commit()
         return "Thanks for the feedback."
 
     if query == "no" or query == "false" or query == "wrong" or query == "bad bot":
         with connection.cursor() as cursor:
-            cursor.execute(feeback_query, False)
+            cursor.execute(feedback_query, False)
             connection.commit()
         return "Ops. Sorry about that. I will try to do better in the future. Thanks for the feedback."
-    
 
 
 def main():
     args = sys.argv[1:]
 
-    relevance_detector = RelevanceDetector()
-
-    if (args and args[0] == "--cli"):
+    if args and args[0] == "--cli":
         client = CliChatInterface()
     else:
         client = DiscordChatInterface()
-    
+
     client.add_message_receiver(get_feedback)
     client.add_message_receiver(answer_query)
-    
-    if (args and args[0] == "--cli"):
+
+    if args and args[0] == "--cli":
         client.get_message()
     else:
         client.run('NTgyNzk1NjcxOTY3NDk4MjUx.XOzByQ.TWGoeHzh5i-LI4dVLWUFmrMmJ5w')
