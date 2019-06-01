bead8119a18b99902a29b07fd5ac57ff4dd1e907
nagasaichandra <nagasaichandra@gmail.com>
2019-05-31 18:46:29 -0700

    added few more variables [blended-requirements], [blended-description], [blended-benefits] in a different function called scrapeBlended()

--- src/scrapers/scrapeMasters.py
+++ src/scrapers/scrapeMasters.py
@@ -71,6 +71,33 @@ def scrapeMasters():
     
     return finalDict
 
+
+def scrapeBlended():
+	final_dict = {}
+	#Intends to scrape variables [blended-requirements], [blended-description], [blended-benefits]
+	url1 = "http://catalog.calpoly.edu/collegesandprograms/collegeofengineering/computersciencesoftwareengineering/#blendedbsmstext"
+	url2 = "http://catalog.calpoly.edu/graduateeducation/#generalpoliciesgoverninggraduatestudiestext"
+	myRequest1 = requests.get(url1, verify=False)
+	myRequest2 = requests.get(url2, verify=False)
+	soup1 = BeautifulSoup(myRequest1.text, "html.parser")
+	soup2 = BeautifulSoup(myRequest2.text, "html.parser")
+	match = re.search(r'Blended BS \+ MS Computer Science Program\n(.*)\nA blended program is available for MS Computer Science.', soup1.get_text())
+	final_dict['blended-benefits'] = match.group(1)
+	#print(soup2.get_text())	
+	match = re.search(r'Eligibility\n\n(.*\n.*\n.*\n.*\n.*\n)\nProcess to Graduate with Both Degrees', soup2.get_text())
+	general_eligibility = match.group(1)
+	#print(soup1.get_text())
+	match = re.search(r'(Majors that are eligible for the blended program are:\n\n.*\n.*\n.*\n\n)Participation', soup1.get_text())
+	cs_blended_eligible_majors = match.group(1)
+	blended_requirements = cs_blended_eligible_majors + general_eligibility
+	final_dict['blended-requirements'] = blended_requirements
+	#print(soup2.get_text())
+	match = re.search(r"Blended Bachelor's \+ Master's Programs\nOverview\n(.*\n.*\n.*\n)Eligibility", soup2.get_text())
+	final_dict['blended-description'] = (match.group(1))
+	return final_dict
+
+
 if __name__ == "__main__":
     print(scrapeMasters())
     # scrapeMasters()
+    print(scrapeBlended())
