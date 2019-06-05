bc376970f309d42efd1c77c4786432c0f640f1ae
Atomic <ltluistrujillo25@gmail.com>
2019-06-05 13:03:07 -0700

    Updated scrapeTransfer to snake case

--- src/scrapers/scrapeTransfer.py
+++ src/scrapers/scrapeTransfer.py
@@ -3,6 +3,7 @@ import re
 import bleach
 import urllib3
 from bs4 import BeautifulSoup
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 """
@@ -12,62 +13,65 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         CSSE-transfer-guidelines
         major-transfer-course-list
 """
-def getTag(row ,string):
-    retList = []
+
+
+def get_tag(row, string):
+    ret_list = []
     for single in row:
         if single.find(string) != None and single.find(string) != -1:
-            retList.append(single.find(string))
-    return retList
-    
+            ret_list.append(single.find(string))
+    return ret_list
 
-def getTagAttrs(row, string, attr, field):
-    retList = []
+
+def get_tag_attrs(row, string, attr, field):
+    ret_list = []
     for single in row:
-        if single.find(string, attrs={str(attr): str(field)}) != None and single.find(string, attrs={str(attr) : str(field)}) != -1:
-            retList.append(single.find(string, attrs={str(attr): str(field)}))
-    return retList
+        if single.find(string, attrs={str(attr): str(field)}) != None and single.find(string, attrs={
+            str(attr): str(field)}) != -1:
+            ret_list.append(single.find(string, attrs={str(attr): str(field)}))
+    return ret_list
 
 
-def cleanHtml(string):
+def clean_html(string):
     return bleach.clean(string, tags=[], strip=True)
 
 
-def scrapeTransfer():
-    finalDict = {}
-    articulatesHtml = []
+def scrape_transfer():
+    final_dict = {}
+    articulates_html = []
 
-    #obtain the content of the URL in HTML
+    # obtain the content of the URL in HTML
     url = "https://admissions.calpoly.edu/computer-science.html"
 
-    myRequest = requests.get(url, verify=False)
+    my_request = requests.get(url, verify=False)
 
-    #Create a soup object that parses the HTML
-    soup = BeautifulSoup(myRequest.text, "html.parser")
+    # Create a soup object that parses the HTML
+    soup = BeautifulSoup(my_request.text, "html.parser")
 
     # gather data
-    for transferRow in soup.find_all('div', attrs={"class": "field-item even"}):
-        transferUnitsHtml = getTag(transferRow, 'u')
-        articulatesHtml = transferRow.find_all('td', attrs={"width": "700"})
-        generalGuidesHtml = transferRow.find_all('p')
-        bulletsHtml = transferRow.find_all('li')
-    
+    for transfer_row in soup.find_all('div', attrs={"class": "field-item even"}):
+        transfer_units_html = get_tag(transfer_row, 'u')
+        articulates_html = transfer_row.find_all('td', attrs={"width": "700"})
+        general_guides_html = transfer_row.find_all('p')
+        bullets_html = transfer_row.find_all('li')
+
     # clean up and split courses
-    articulates = "\n".join(list(map(cleanHtml, articulatesHtml)))
+    articulates = "\n".join(list(map(clean_html, articulates_html)))
     articulates = re.split(
         r"Major Related 1\*", str(articulates))
-    
-    matchObj = re.search(r"(Selection.*alone\.)",
-                         "\n".join(list(map(cleanHtml, generalGuidesHtml))))
 
-    finalDict['transfer-min-units'] = cleanHtml(transferUnitsHtml[0])
-    finalDict['transfer-articulate-courses'] = articulates[1]
-    finalDict['CSSE-transfer-guidelines'] = matchObj.group(1) 
-    # + "\n" + cleanHtml(bulletsHtml)
-    finalDict['major-transfer-course-list'] = "Major Related 1*\n" + articulates[0]
-    return finalDict
+    match_obj = re.search(r"(Selection.*alone\.)",
+                          "\n".join(list(map(clean_html, general_guides_html))))
+
+    final_dict['transfer-min-units'] = clean_html(transfer_units_html[0])
+    final_dict['transfer-articulate-courses'] = articulates[1]
+    final_dict['CSSE-transfer-guidelines'] = match_obj.group(1)
+    # + "\n" + clean_html(bullets_html)
+    final_dict['major-transfer-course-list'] = "Major Related 1*\n" + articulates[0]
+    return final_dict
 
 
 if __name__ == "__main__":
-    # print(scrapeTransfer())
-    final = scrapeTransfer()
+    # print(scrape_transfer())
+    final = scrape_transfer()
     print(final['CSSE-transfer-guidelines'])
