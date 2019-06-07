57601ba18fd52817b2077861dc5603b6cebf8f34
nagasaichandra <nagasaichandra@gmail.com>
2019-06-07 13:32:59 -0700

    updated database for contacts questions

--- src/scrapers/scrape_all.py
+++ src/scrapers/scrape_all.py
@@ -4,7 +4,8 @@ from .scrape_cs_minor import scrape_cs_minor
 from .scrape_masters import scrape_masters
 from .scrape_objectives_and_missions import scrapeObjectivesandMissions
 from .scrape_transfer import scrape_transfer
-
+from .scrape_masters import scrape_blended
+from .scrape_contact import scrape_contact
 
 def scrape_all():
     """ Scrapes all the data sources and puts the data into the database """
@@ -14,7 +15,8 @@ def scrape_all():
     scrape_masters()
     scrapeObjectivesandMissions()
     scrape_transfer()
-
+    scrape_blended()
+    scrape_contact()
 
 if __name__ == '__main__':
     scrape_all()
--- src/scrapers/scrape_contact.py
+++ src/scrapers/scrape_contact.py
@@ -2,42 +2,72 @@ import requests
 from bs4 import BeautifulSoup
 import urllib3
 import re
+from ..database_connection import connection
 
 
+def scrape_contact_main():
+    """https://statistics.calpoly.edu/data-science-minor ===> DATA SCIENCE MINOR
+    CSC Minor advisors contact available in a pdf at https://csc.calpoly.edu/static/media/uploads/computer_science_&_software_engineering_faculty_advisor_list_8-28-14.pdf
+    Scrapes variables [[department-contact-info], [phone-number], [minor-advisors]]
+    """
+    global i
+    urllib3.disable_warnings()
+    url1 = "https://csc.calpoly.edu/contact/"
+    myRequest1 = requests.get(url1, verify=False)
+    soup1 = BeautifulSoup(myRequest1.text,"html.parser")
+    url2 = "https://statistics.calpoly.edu/data-science-minor"
+    myRequest2 = requests.get(url2, verify=False)
+    soup2 = BeautifulSoup(myRequest2.text,"html.parser")
+    final_dict = {}
+    match = re.search(r'Contact Information\n(.*)\n(.*)', soup1.get_text())
+    department_contact = match.group(1)
+    department_office_hours = match.group(2)
+    match = re.search(r'Phone: (\d\d\d-\d\d\d-\d\d\d\d) ', department_contact)
+    final_dict['phone-number'] = match.group(1)
+    final_dict['department-contact-info'] = department_contact
+    final_dict['department-office-hours'] = department_office_hours
+    minor_advisors = {}
+    cs_advisors = ['Bellardo', 'Clements', 'Dekhtyar', 'Gharibyan', 'Haung', 'Kearns', 'Keen', 'Khosmood', 'Kurfess', 'Lupo', 'Nico', 'Peterson', 'Seng', 'Staley', 'Sueda']
+    cs_minor_string = ''
+    for i in cs_advisors:
+        cs_minor_string + i
+    match = re.search(r'Minor Advising\n(.*)', soup2.get_text())
+    minor_advisors['ds-minor'] = match.group(1)
+    final_dict['minor-advisors'] = minor_advisors
+    # print(final_dict)
+    return final_dict
+
+
+def ingest_contact_basic(contacts):
+    with connection.cursor() as cursor:
+        cursor.execute('''INSERT INTO contact_basic VALUES ("%s", "%s", "%s", "%s");''' % (contacts['phone-number'],
+                                                                                           contacts['department-contact-info'],
+                                                                                           contacts['department-contact-info'],
+                                                                                           contacts['department-office-hours']))
+        cursor.commit()
+
+
+def ingest_contact_minor_advisors(contacts):
+    with connection.cursor() as cursor:
+        for minor in contacts['minor-advisors']:
+            cursor.execute('''INSERT INTO contact_minor_advisors VALUES ("%s", "%s");''' % (minor, contacts['minor-advisors'][minor]))
+        cursor.commit()
+
+
+def remove_content():
+    with connection.cursor() as cursor:
+
+        cursor.execute('''TRUNCATE TABLE contact_basic''')
+        cursor.execute('''TRUNCATE TABLE contact_minor_advisors''')
+        cursor.commit()
+
+# scrape_contact_main()
 def scrape_contact():
-	"""
-		https://statistics.calpoly.edu/data-science-minor ===> DATA SCIENCE MINOR
-		CSC Minor advisors contact available in a pdf at https://csc.calpoly.edu/static/media/uploads/computer_science_&_software_engineering_faculty_advisor_list_8-28-14.pdf
-		Scrapes variables [[department-contact-info], [phone-number], [minor-advisors]]
-	"""
+    contacts = scrape_contact_main()
+    ingest_contact_basic(contacts)
+    ingest_contact_minor_advisors(contacts)
+    remove_content()
 
-	urllib3.disable_warnings()
-	
-	url1 = "https://csc.calpoly.edu/contact/"
-	myRequest1 = requests.get(url1, verify=False)
-	soup1 = BeautifulSoup(myRequest1.text,"html.parser")
-	
-	url2 = "https://statistics.calpoly.edu/data-science-minor"
-	myRequest2 = requests.get(url2, verify=False)
-	soup2 = BeautifulSoup(myRequest2.text,"html.parser")
-	
-	final_dict = {}
-	match = re.search(r'Contact Information\n(.*)\n(.*)', soup1.get_text())
-	department_contact = match.group(1)
-	department_office_hours = match.group(2)
-	match = re.search(r'Phone: (\d\d\d-\d\d\d-\d\d\d\d) ', department_contact)
-	final_dict['phone-number'] = match.group(1)
-	final_dict['department-contact-info'] = department_contact
-	final_dict['department-office-hours'] = department_office_hours
-	
-	minor_advisors = {}
-	minor_advisors['cs-minor'] = ['Bellardo', 'Clements', 'Dekhtyar', 'Gharibyan', 'Haung', 'Kearns', 'Keen', 'Khosmood', 'Kurfess', 'Lupo', 'Nico', 'Peterson', 'Seng', 'Staley', 'Sueda']
-	
-	match = re.search(r'Minor Advising\n(.*)', soup2.get_text())
-	minor_advisors['ds-minor'] = match.group(1)
-	final_dict['minor-advisors'] = minor_advisors
-	return final_dict
-	
 if __name__ == "__main__":
-	print(scrape_contact())
+    scrape_contact()
 
