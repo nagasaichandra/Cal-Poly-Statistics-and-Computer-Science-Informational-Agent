8e5353287c0306f03ee77a68d142ebc433d9bd41
Atomic <ltluistrujillo25@gmail.com>
2019-06-08 18:00:30 -0700

    Added attributes to minor db table

--- src/sql/createTableMinor.sql
+++ src/sql/createTableMinor.sql
@@ -1,5 +1,12 @@
 CREATE TABLE cs_minor_info (
     division_units              TEXT,
     minor_general_requirements  TEXT,
-    minor_flowchart_link        TEXT
+    minor_flowchart_link        TEXT,
+    required_courses TEXT,
+    steps TEXT,
+    minimum_gpa TEXT,
+    prerequisites TEXT,
+    application_link TEXT,
+    required_units TEXT,
+    approved_elective_units TEXT
 );
